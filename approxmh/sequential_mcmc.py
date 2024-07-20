9ebc13c461de37bcfd659eae9520cb43d7c008e3
Yur3k <yur3kski@gmail.com>
2024-07-20 23:34:15 +0300

    Implemented Annealed Importance Sampling

--- approxmh/sequential_mcmc.py
+++ approxmh/sequential_mcmc.py
@@ -1,4 +1,6 @@
 import torch
+import math
+from abc import ABC, abstractmethod
 
 
 class DensityMixture:
@@ -12,16 +14,60 @@ class DensityMixture:
         return self.power1 * self.distribution1.log_prob(x) + self.power2 * self.distribution2.log_prob(x)
 
 
+class MarkovKernel(ABC):
+    @abstractmethod
+    def step(self, x, n_steps=1):
+        '''
+        Apply the kernel to x `n_steps` times
+        '''
+        raise NotImplementedError
+
+    @abstractmethod
+    def log_prob(self, x, y):
+        '''
+        Return the transition density from x to y if it exists
+        '''
+        raise NotImplementedError
+
+class ULAKernel(MarkovKernel):
+    def __init__(self, stationary_distribution, time_step):
+        super().__init__()
+        self.negative_energy = stationary_distribution.log_prob
+        self.time_step = time_step
+
+    def step(self, x):
+        minus_grad_U = self._grad_negative_energy(x)
+        noise = torch.randn_like(x)
+        y = x + self.time_step * minus_grad_U + math.sqrt(2 * self.time_step) * noise
+        return y
+
+    def log_prob(self, x, y):
+        assert(x.shape == y.shape)
+        minus_grad_U = self._grad_negative_energy(x)
+        standard_normal = torch.distributions.Normal(0., 1.)
+        return standard_normal.log_prob((y - x - self.time_step * minus_grad_U) / math.sqrt(2 * self.time_step)).view(x.shape[0], -1).sum(axis=1)
+
+    def _grad_negative_energy(self, x):
+        x = x.clone()
+        x.requires_grad_()
+        x.grad = None
+        self.negative_energy(x).sum().backward()
+        return x.grad
+
+
 def run_annealed_importance_sampling(
     p_0,
     p_n,
-    n : int,
-    K : int,
+    n_steps : int,
+    n_particles : int,
     transition_kernel,
-    n_kernel_steps : int = 1
 ):
     '''
     Use annealed importance sampling to generate a weighted sample from p_N using samples from p_0
+    
+    This function works with tractable (allowing transition density evaluation) forward kernels and
+    uses $L_{k-1}(x_k, x_{k-1})=M_n(x_{k-1}, x_{k})$ as reverse kernels, where $M$ denotes forward
+    kernels.
     
     Parameters
     ----------
@@ -29,37 +75,36 @@ def run_annealed_importance_sampling(
         Unnormalized starting distribution from which we can sample
     p_n
         Unnormalized target distribution from which we wish to sample
-    n
+    n_steps
         Number of interpolating steps
-    K
+    n_particles
         Number of particles
     transition_kernel
         Function that returns a Markov kernel with a given stationary distribution
-    n_kernel_steps
-        Number of times a kernel is applied at each interpolating step
     Returns
     -------
     (torch.tensor, torch.tensor)
-        Weighted sample from p_N (log weights, values)
+        Weighted sample from p_N (log_weights, values)
+        The expected value of each weight is the ratio of the normalizing constants of p_n and p_0
     '''
     # Annealing schedule
-    beta = torch.linspace(0, 1, n + 1)
+    beta = torch.linspace(0, 1, n_steps + 1)
     # Intermediate distributions
-    gamma = [DensityMixture(p_0, 1 - beta[t], p_n, beta[t]) for t in range(n + 1)]
+    gamma = [DensityMixture(p_0, 1 - beta[t], p_n, beta[t]) for t in range(n_steps + 1)]
     # Particles
-    X = p_0.sample((K,))
-    # Logarithms of weights
+    X = p_0.sample((n_particles,))
+    # Logarithmic weights
     logW = -p_0.log_prob(X)
-
-    for t in range(1, n + 1):
-        kernel = transition_kernel(gamma[t])
+    for t in range(1, n_steps+ 1):
+        # Markov kernel with stationary distribution gamma_t
+        M_t = transition_kernel(gamma[t])
         X_next = X
-        for i in range(n_kernel_steps):
-            X_next = kernel(X_next)
+        X_next = M_t.step(X_next)
         # Incremental importance weights
-        logW += gamma[t - 1].log_prob(X) - gamma[t - 1].log_prob(X_next)
-        
+        logW += M_t.log_prob(X_next, X) - M_t.log_prob(X, X_next)
+        X = X_next
     logW += p_n.log_prob(X)
+    return logW, X
 
 
 
