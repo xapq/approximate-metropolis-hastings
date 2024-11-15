Fixed VAE on MNIST Decoder (conditional) distribution now has uniform trainable variance for all pixels instead of independent trainable variance for each pixel.

approxmh/model_trainers.py
@@ -69,15 +69,7 @@ class ModelTrainer:
         self.model.train(is_train)
         sum_losses = 0
         with torch.set_grad_enabled(is_train):
-            '''with torch.profiler.profile(
-                    schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
-                    on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/vae_mnist_training'),
-                    record_shapes=True,
-                    profile_memory=True,
-                    with_stack=True
-            ) as prof:'''
             for x in tqdm(data_loader):
-                # prof.step()
                 x = x.to(self.model.device)
                 loss = self.loss(x)
                 if is_train:
@@ -135,8 +127,8 @@ class AdaptiveVAETrainer(ModelTrainer):
         batch_dim = x.shape[0]
         # print(x.shape, log_var_recon_x.shape)
         reconstruction_loss = -mean_field_log_prob(
-            (x - mean_recon_x).view(batch_dim, -1), 
-            log_var_recon_x.exp().view(batch_dim, -1)
+            (x - mean_recon_x).flatten(start_dim=1), # .view(batch_dim, -1)
+            log_var_recon_x.exp().flatten(start_dim=1)
         )
         kl_divergence = -0.5 * (1 + log_var_z - mean_z.pow(2) - log_var_z.exp()).sum(dim=1)
         return reconstruction_loss, kl_divergence

approxmh/vae.py
@@ -301,16 +301,21 @@ class ConvVAE(VAE):
             nn.BatchNorm2d(conv1_out_channels),
             nn.ReLU(),
             nn.Upsample(prepool_dim1),
-            nn.ConvTranspose2d(conv1_out_channels, 2, kernel_size, stride, padding)
+            nn.ConvTranspose2d(conv1_out_channels, 1, kernel_size, stride, padding)
         )
+        
+        # EXPERIMENTAL
+        self.decoder_log_var = nn.Parameter(torch.tensor(-2.))
 
     def __repr__(self):
         return f'convvae_latent{self.latent_dim}'
 
     def decoding_parameters(self, z):
-        # mean_x, log_var_x = torch.unbind(self.decoder(z), dim=-3)
-        mean_x, log_var_x = torch.split(self.decoder(z), 1, dim=-3)
-        # mean_x = 2 * torch.sigmoid(mean_x) - 1
+        #$$$ mean_x, log_var_x = torch.unbind(self.decoder(z), dim=-3)
+        # mean_x, log_var_x = torch.split(self.decoder(z), 1, dim=-3)
+        mean_x = self.decoder(z)
+        log_var_x = self.decoder_log_var.expand(mean_x.shape)
+        #$$$ mean_x = 2 * torch.sigmoid(mean_x) - 1
         return mean_x, log_var_x
 
 

approxmh/y_utils.py
@@ -138,7 +138,7 @@ def mean_field_log_prob(
     #    raise ValueError("var is of incorrect size")
 
     # Entries of var must be non-negative
-    ### CHECKING THIS TAKES 97% OF TOTAL mean_field_log_prob TIME
+    ### CHECKING THIS CAUSES CUDA GPU SYNC WHICH MUST BE AVOIDED AT ALL COSTS
     # if torch.any(var < 0):
     #    raise ValueError("var has negative entry/entries")
 